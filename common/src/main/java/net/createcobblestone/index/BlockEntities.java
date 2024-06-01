0dd06162bfcf4365736310a709cf64a00ed60430
sticky <mr.sticky.piston@gmail.com>
2024-06-01 11:15:11 +0200

    fixed crashing after disabling generators

--- common/src/main/java/net/createcobblestone/index/BlockEntities.java
+++ common/src/main/java/net/createcobblestone/index/BlockEntities.java
@@ -1,26 +1,55 @@
 package net.createcobblestone.index;
 
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
-import com.tterrag.registrate.util.entry.BlockEntry;
+import com.tterrag.registrate.util.nullness.NonNullSupplier;
 import net.createcobblestone.CreateCobblestoneMod;
-import net.createcobblestone.blocks.CobblestoneGeneratorBlock;
 import net.createcobblestone.blocks.CobblestoneGeneratorBlockEntity;
 import net.createcobblestone.blocks.CobblestoneGeneratorInstance;
 import net.createcobblestone.blocks.CobblestoneGeneratorRenderer;
 import net.minecraft.world.level.block.Block;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static net.createcobblestone.CreateCobblestoneMod.REGISTRATE;
+import static net.createcobblestone.index.Blocks.*;
 
 public class BlockEntities {
-    public static BlockEntityEntry<CobblestoneGeneratorBlockEntity> COBBLESTONE_GENERATOR = REGISTRATE
-            .blockEntity("cobblestone_generator", CobblestoneGeneratorBlockEntity::new)
-            .instance(() -> CobblestoneGeneratorInstance::new)
-            .validBlocks(Blocks.COBBLESTONE_GENERATOR_BLOCK, Blocks.STONE_GENERATOR_BLOCK, Blocks.BASALT_GENERATOR_BLOCK, Blocks.LIMESTONE_GENERATOR_BLOCK, Blocks.SCORIA_GENERATOR_BLOCK)
-            .renderer(() -> CobblestoneGeneratorRenderer::new)
-            .register();
+    public static BlockEntityEntry<CobblestoneGeneratorBlockEntity> COBBLESTONE_GENERATOR;
 
     public static void init() {
         CreateCobblestoneMod.LOGGER.info("Registering blockEntities for " + CreateCobblestoneMod.NAME);
+        List<NonNullSupplier<? extends Block>> suppliers = new ArrayList<>();
 
+        if (Config.common().cobblestoneGeneratorEnabled.get()) {
+            suppliers.add(COBBLESTONE_GENERATOR_BLOCK);
+        }
+
+        if (Config.common().stoneGeneratorEnabled.get()) {
+            suppliers.add(STONE_GENERATOR_BLOCK);
+        }
+
+        if (Config.common().basaltGeneratorEnabled.get()) {
+            suppliers.add(BASALT_GENERATOR_BLOCK);
+
+        }
+
+        if (Config.common().limestoneGeneratorEnabled.get()) {
+            suppliers.add(LIMESTONE_GENERATOR_BLOCK);
+        }
+
+        if (Config.common().scoriaGeneratorEnabled.get()) {
+            suppliers.add(SCORIA_GENERATOR_BLOCK);
+        }
+
+        @SuppressWarnings("unchecked")
+        NonNullSupplier<? extends Block>[] suppliersArray = suppliers.toArray(new NonNullSupplier[0]);
+
+        COBBLESTONE_GENERATOR = REGISTRATE
+                .blockEntity("cobblestone_generator", CobblestoneGeneratorBlockEntity::new)
+                .instance(() -> CobblestoneGeneratorInstance::new)
+                .validBlocks(suppliersArray)
+                .renderer(() -> CobblestoneGeneratorRenderer::new)
+                .register();
     }
 }
