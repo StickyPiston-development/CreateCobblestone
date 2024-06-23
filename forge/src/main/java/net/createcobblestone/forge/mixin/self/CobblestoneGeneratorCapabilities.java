2a1ec7bb326531661950f01778cb8b6ded929e15
sticky <mr.sticky.piston@gmail.com>
2024-06-23 09:33:38 +0200

    made it work!

---
+++ forge/src/main/java/net/createcobblestone/forge/mixin/self/CobblestoneGeneratorCapabilities.java
@@ -0,0 +1,52 @@
+package net.createcobblestone.forge.mixin.self;
+
+import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
+import net.createcobblestone.blocks.CobblestoneGeneratorBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.Container;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(CobblestoneGeneratorBlockEntity.class)
+public abstract class CobblestoneGeneratorCapabilities extends KineticBlockEntity implements ICapabilityProvider, Container {
+    public CobblestoneGeneratorCapabilities(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Unique
+    LazyOptional<? extends IItemHandler> createcobblestoneinv$handler = LazyOptional.of(() -> new InvWrapper(this));
+
+    @Override
+    public <T> @NotNull LazyOptional<T> getCapability(@NotNull Capability<T> capability, @Nullable Direction facing) {
+        if (capability == ForgeCapabilities.ITEM_HANDLER && !remove) {
+            // If down return the down handler otherwise return the one for all other sides
+            return createcobblestoneinv$handler.cast();
+        }
+
+        return super.getCapability(capability, facing);
+    }
+
+
+    @Override
+    public void reviveCaps() {
+        super.reviveCaps();
+        createcobblestoneinv$handler = LazyOptional.of(() -> new InvWrapper(this));
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        createcobblestoneinv$handler.invalidate();
+    }
+}
